86ecb0de559cb4494f58ee26e095bd961297d7cd
SalmonFillet <sy4.l3ucas@gmail.com>
2022-06-15 23:30:23 +0900

    ✨ impl bootstrap

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -1,5 +1,48 @@
 import React from 'react';
 
 export const BlogPage = () => {
-  return <div>BlogPage</div>;
+  return (
+    <div className="container">
+      <h1>Blog</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, natus
+        aut! Quas ipsam culpa recusandae eaque in a aliquid atque laborum,
+        consectetur, officiis accusantium ut perferendis sunt sapiente
+        asperiores consequuntur. Neque eligendi autem quo animi, assumenda
+        magnam aut reprehenderit, distinctio obcaecati totam amet praesentium
+        commodi vitae repellat placeat dolor nam laboriosam fuga soluta
+        perspiciatis omnis aspernatur atque est? Amet officia in necessitatibus
+        odit ex qui eveniet iste explicabo a doloremque voluptatem harum nihil
+        enim quasi, provident accusamus possimus nulla similique sint tempora.
+        Nam sit sunt consequatur est! Cumque inventore pariatur, delectus quod
+        ipsa ducimus possimus quaerat mollitia magni! Iste, atque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, natus
+        aut! Quas ipsam culpa recusandae eaque in a aliquid atque laborum,
+        consectetur, officiis accusantium ut perferendis sunt sapiente
+        asperiores consequuntur. Neque eligendi autem quo animi, assumenda
+        magnam aut reprehenderit, distinctio obcaecati totam amet praesentium
+        commodi vitae repellat placeat dolor nam laboriosam fuga soluta
+        perspiciatis omnis aspernatur atque est? Amet officia in necessitatibus
+        odit ex qui eveniet iste explicabo a doloremque voluptatem harum nihil
+        enim quasi, provident accusamus possimus nulla similique sint tempora.
+        Nam sit sunt consequatur est! Cumque inventore pariatur, delectus quod
+        ipsa ducimus possimus quaerat mollitia magni! Iste, atque.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat, natus
+        aut! Quas ipsam culpa recusandae eaque in a aliquid atque laborum,
+        consectetur, officiis accusantium ut perferendis sunt sapiente
+        asperiores consequuntur. Neque eligendi autem quo animi, assumenda
+        magnam aut reprehenderit, distinctio obcaecati totam amet praesentium
+        commodi vitae repellat placeat dolor nam laboriosam fuga soluta
+        perspiciatis omnis aspernatur atque est? Amet officia in necessitatibus
+        odit ex qui eveniet iste explicabo a doloremque voluptatem harum nihil
+        enim quasi, provident accusamus possimus nulla similique sint tempora.
+        Nam sit sunt consequatur est! Cumque inventore pariatur, delectus quod
+        ipsa ducimus possimus quaerat mollitia magni! Iste, atque.
+      </p>
+    </div>
+  );
 };
